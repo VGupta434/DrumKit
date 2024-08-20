7eae77cbf4bff2c4422e4243d8bbdffb4a8ccae8
Vivek <vg52531@gmail.com>
2024-08-20 23:46:04 +0530

    completed dev

--- index.js
+++ index.js
@@ -1,36 +1,58 @@
 // alert("welcome to drum kit website");
 document.querySelectorAll(".drum").forEach((element) => {element.addEventListener("click", () => {playSound(element.innerHTML)})});
-function playSound(button)
+
+document.addEventListener("keydown", (event) => {
+    // alert(event.key+" key was pressed");
+    // console.log(event);
+    playSound(event.key);
+});
+
+
+function playSound(buttonKey)
 {
-    console.log(button);
-    switch(button)
+
+    
+    buttonKey = buttonKey.toLowerCase();
+    let drumKeys = ['w', 'a', 's', 'd', 'j', 'k', 'l'];
+    if(drumKeys.includes(buttonKey))
     {
-        case "w":
-            console.log("w pressed");
-            let tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play;
-            break;
-        case "a":
-            console.log("a pressed");
-            let tom2 = new Audio('sounds/tom-2.mp3');
-            tom2.play;
-            break;
-        case "s":
-            console.log("s pressed");
-            break;
-        case "d":
-            console.log("d pressed");
-            break;
-        case "j":
-            console.log("j pressed");
-            break;
-        case "k":
-            console.log("k pressed");
-            break;
-        case "l":
-            console.log("l pressed");
-            break;
-        default :
-            console.log("no such button");
+        button = document.querySelector("."+buttonKey);
+        button.classList.add("pressed");
+        setTimeout(() => {button.classList.remove("pressed")}, 80);
+        switch(buttonKey)
+        {
+            case "w":
+                let tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "a":
+                let tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case "s":
+                let tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case "d":
+                let tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            case "j":
+                let snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            case "k":
+                let crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            case "l":
+                let kick = new Audio('sounds/kick-bass.mp3');
+                kick.play();
+                break;
+            default :
+                console.log("no such button");
+        }
     }
+    // console.log(buttonKey);
+    
 }
